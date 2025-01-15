39b49f9bc004d642aa4a8b79781b63c2574a615e
hanbiao <hanbiao4079@gmail.com>
2025-01-15 16:19:21 +0800

    初始化

---
+++ packages/alter-ui/vite.config.ts
@@ -0,0 +1,22 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "alter-ui",
+      fileName: (format) => `my-react-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
